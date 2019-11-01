eccdacd50aa2c8e679c99ce821732ed12045704d
olyashenrik <olshendrik4@gmail.com>
2019-11-01 15:33:47 +0200

    adding file _section-week-trending.scss, adding changes in file index.html, _listing.scss, _section-week-tranding.scss and _section-hero.scss; adding file card-ladel.png to directory 'img'

---
+++ js/main.js
@@ -0,0 +1,56 @@
+/*
+* isotope init begin
+*/
+var $grid = $('.grid').isotope({
+    // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+});
+// hash of functions that match data-filter values
+var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function() {
+        // use $(this) to get item element
+        var number = $(this).find('.number').text();
+        return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function() {
+        var name = $(this).find('.name').text();
+        return name.match( /ium$/ );
+    }
+};
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    // use filter function if value matches
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+});
+/*
+* isotope init end
+*/
+$('#recipeCarousel').carousel({
+    interval: 10000
+})
+
+$('.carousel .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i=0;i<2;i++) {
+        next=next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
+
